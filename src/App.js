584112506e9495b5b33ae865d884fd5e07b30f1b
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-05-28 00:00:17 +0200

    Add alert notification

--- src/App.js
+++ src/App.js
@@ -7,26 +7,33 @@ const App = () => {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+  const [alert, setAlert] = useState({
+    show: false,
+    type: '',
+    msg: '',
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      // display alert
+      showAlert(true, 'danger', 'please enter value');
     } else if (name && isEditing) {
-      // deal with edit
+      setAlert({show: true, msg: 'please enter value', type: 'danger'});
     } else {
-      // show alert
       const newItem = {id: new Date().getTime().toString(), title: name}
       setList([...list, newItem]);
       setName('');
     }
   }
 
+  const showAlert = (show=false, type='', msg='') => {
+    setAlert({show, type, msg})
+  }
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert />} 
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />} 
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
